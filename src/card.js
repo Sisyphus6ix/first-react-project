721bcb6234275ced35f1e29e210ff6ba833c017f
Ryan Silvera <ryan.silvera@icloud.com>
2023-05-28 22:46:31 -0400

    First React Project

---
+++ src/card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='LeBron James' src="https://media.gettyimages.com/id/1211221823/photo/los-angeles-lakers-v-los-angeles-clippers.jpg?s=1024x1024&w=gi&k=20&c=CAAoqh2grRfi_VBLtw93B8C1oRDKkzPCFcUtbGplOPk="></img>
+            <div>
+                <h2>Jane doe</h2>
+                <p>Janedoe@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { Hello } from './Hello';
+import Card from './card';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Hello />
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<Card />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
